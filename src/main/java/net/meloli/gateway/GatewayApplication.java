24de6560cff25d8ce840ac06321939ea63b31900
LiRETRO <hash@meloli.net>
2018-12-11 18:03:35 +0800

    配置网关

--- src/main/java/net/meloli/gateway/GatewayApplication.java
+++ src/main/java/net/meloli/gateway/GatewayApplication.java
@@ -2,10 +2,11 @@ package net.meloli.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringCloudApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
