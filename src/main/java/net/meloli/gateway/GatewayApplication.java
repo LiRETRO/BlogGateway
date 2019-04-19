151ac8dae523d5b500f33a51d41093966d63803f
LiRETRO <hash@meloli.net>
2019-04-19 20:55:02 +0800

    更改

--- src/main/java/net/meloli/gateway/GatewayApplication.java
+++ src/main/java/net/meloli/gateway/GatewayApplication.java
@@ -4,9 +4,11 @@ import net.meloli.gateway.config.SpringHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringCloudApplication
 public class GatewayApplication {
 
--- src/test/java/net/meloli/gateway/GatewayApplicationTests.java
+++ src/test/java/net/meloli/gateway/GatewayApplicationTests.java
@@ -1,9 +1,14 @@
 package net.meloli.gateway;
 
+import net.meloli.gateway.config.CommandHelloWorld;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,18 @@ public class GatewayApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
+
+    @Test
+    public void testAsynchronous2() throws Exception {
+
+        Future<String> fWorld = new CommandHelloWorld("World").queue();
+        Future<String> fBob = new CommandHelloWorld("Bob").queue();
+
+        assertEquals ("HelloWorld", fWorld.get());
+        assertEquals("HelloBob", fBob.get());
+    }
+
 
 }
